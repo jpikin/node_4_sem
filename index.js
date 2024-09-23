e242d656834d65993028f64dbaf7bb22ec28092f
Pikin <pikin@bk.ru>
2024-09-23 15:41:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+const users = [];
+
+let ID = 0;
+
+app.use(express.json());
+
+app.get('/users', (req, res)=>{
+    res.send({users});
+});
+
+// app.get('/users/:id', (req, res)=>{
+//     const user = [];
+// });
+
+app.post('/users', (req, res)=>{
+    let user = {
+        id : ID++,
+        ...req.body
+    }
+    users.push(user);
+    res.send({user});
+
+});
+
+app.put('/users/:id', (req, res)=>{
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+    if(user){
+        const {name, age} = req.body;
+        user.name = name,
+        user.age = age
+        res.send({user})
+    } else {
+        res.status(404).send({user : null});
+    }
+});
+app.listen(3000);
