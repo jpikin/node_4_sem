cc72c98594b745fd52e0faa599d79e495c7873dc
Pikin <pikin@bk.ru>
2024-09-23 15:59:51 +0500

    delete added

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ app.get('/users', (req, res)=>{
     res.send({users});
 });
 
-// app.get('/users/:id', (req, res)=>{
-//     const user = [];
-// });
+ app.get('/users/:id', (req, res)=>{
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+    
+    user ? res.send({user}) : res.status(404).send({user : null});
+    
+});
 
 app.post('/users', (req, res)=>{
     let user = {
@@ -33,6 +37,20 @@ app.put('/users/:id', (req, res)=>{
         const {name, age} = req.body;
         user.name = name,
         user.age = age
+        res.send({user})
+    } else {
+        res.status(404).send({user : null});
+    }
+});
+
+
+app.delete('/users/:id', (req, res)=>{
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+    
+    if(user){
+        const userIndex = users.indexOf(user);
+        users.splice(userIndex, 1);
         res.send({user})
     } else {
         res.status(404).send({user : null});
